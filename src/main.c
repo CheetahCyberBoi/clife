8368d394ef8c78c33ee04d4b919ab763eefe6950
Cheetah <DaverDiscord@gmail.com>
2024-07-14 19:58:02 -0700

    use an array instead of a linked list because i am dum:tm:

--- src/main.c
+++ src/main.c
@@ -26,13 +26,12 @@ typedef enum {
 typedef struct cell_t{
   cell_state_e state; //The current state of this cell.
   int pos_x, pos_y;   //A cache of this cell's position in the grid.
-  struct cell_t* next;       //A pointer to the next cell in the linked list.
 } cell_t;
 
 //The entire app's state.
 typedef struct {
   struct grid_t {
-    cell_t* first_cell;    //The linked list of cells.
+    cell_t* cells;    //The linked list of cells.
     int num_cells;         //The total number of cells, equal to size_x * size_y.
 
     size_t size_x, size_y; //Dimensions of the grid.
@@ -46,36 +45,7 @@ typedef struct {
 
 
 
-//Adds the given cell to the cell linked list.
-void add_to_cell_ll(cell_t* head, cell_state_e state, int x, int y) {
-  cell_t* current = head;
 
-  //go to the end of the cell linked-list
-  while (current->next != NULL) {
-    current = current->next;
-  }
-
-  //add a new cell on the end
-  current->next = (cell_t*) malloc(sizeof(cell_t));
-  current->next->state = state;
-  current->next->pos_x = x;
-  current->next->pos_y = y;
-  current->next->next = NULL;
-}
-
-//Retrieves a COPY of a cell in a linked list.
-//TODO: figure out how to get the actual cell.
-cell_t* get_cell_ll(cell_t* head, int index) {
-  cell_t* current = head;
-
-  int count = 0;
-  while (count != index && current->next != NULL) {
-    current = current->next;
-    count++;
-  }
-
-  return current;
-}
 #ifdef CLIFE_DBG
 //DEBUG: Prints a cell to the terminal.
 void dbg_print_cell(cell_t cell) {
@@ -83,16 +53,14 @@ void dbg_print_cell(cell_t cell) {
   printf("\tState: %d\n", cell.state);
   printf("\tXpos: %d\n", cell.pos_x);
   printf("\tYpos: %d\n", cell.pos_y);
-  printf("\tNext cell (pointer): %d\n", cell.next);
 }
 
 //DEBUG: prints the cell grid to the terminal.
 void dbg_print_grid(state_t* state) {
   for (int i = 0; i < state->grid.size_x; i++) {
     for (int j = 0; j < state->grid.size_y; j++) {
-      cell_t* cell = get_cell_ll(state->grid.first_cell, j*state->grid.size_y + i);
-      if (cell == NULL) { fprintf(stderr, "clife: debug gridprint cell not found"); }
-      switch (cell->state) {
+      cell_t cell = state->grid.cells[j * state->grid.size_x + i];
+      switch (cell.state) {
         case STATE_DEAD: putchar(' ');
             break;
         case STATE_ALIVE: putchar('@');
@@ -106,33 +74,30 @@ void dbg_print_grid(state_t* state) {
 
 //Initializes the cell linked-list and grid, allocating it on the heap and giving a pointer to the first one. 
 void init_grid(state_t* state, size_t size_x, size_t size_y) {
-  cell_t* cell_alloc = (cell_t*)malloc(sizeof(cell_t));
+  cell_t* cell_alloc = (cell_t*)malloc(sizeof(cell_t) * size_x * size_y);
 
   if (cell_alloc == NULL) {
     perror("clife: failed to allocate space for cell grid");
     exit(1);
   }
-  cell_alloc->state = STATE_DEAD;
-  cell_alloc->pos_x = 0;
-  cell_alloc->pos_y = 0;
-  cell_alloc->next = NULL;
-  
+  memset((void*)cell_alloc, 0, sizeof(cell_t) * size_x * size_y);  
   //Safely have the head of the list - time to go
   for (int i = 0; i < size_x * size_y; i++) {
     //printf("On cell index: %d, coords: (%d, %d)", i, i % size_x, i % size_y);
-    add_to_cell_ll(cell_alloc, STATE_DEAD, i % size_x, i / size_y);
+    cell_t cell = (cell_t) {
+      STATE_DEAD,
+      i % size_x,
+      i / size_y
+    };
+    cell_alloc[i] = cell;
   }
   //Finished initalizing it, time to give it back to the state
-  state->grid.first_cell = cell_alloc;
+  state->grid.cells = cell_alloc;
   state->grid.num_cells = size_x * size_y;
   state->grid.size_x = size_x;
   state->grid.size_y = size_y;
 }
 
-//Destroys the cell linked list at the end of the program.
-void destroy_cell_ll(state_t* state) {
-  free(state->grid.first_cell);
-}
 
 //STATE-RELEATED FUNCTIONS
 
@@ -186,7 +151,7 @@ void update(state_t* state) {
 
 //Cleanup everything after we're done
 void clean_up(state_t* state) {
-  destroy_cell_ll(state);
+  free(state->grid.cells); 
 }
 
 
@@ -194,7 +159,8 @@ int main(int argc, char** argv) {
   state_t state = initialize((size_t)8, (size_t)8);
   printf("We initialized!");
   //make funni pattern
-  for(cell_t* cell = state.grid.first_cell; cell != NULL; cell = cell->next) {
+  for(int i = 0; i < state.grid.size_x * state.grid.size_y; i++) {
+    cell_t* cell = &state.grid.cells[i];
     cell->state = ((cell->pos_x * cell->pos_y) % 2 == 0) ? STATE_ALIVE : STATE_DEAD;
   }
   printf("grid vis:\n");
